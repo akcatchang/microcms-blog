449f817ca1b5b89d744447b035b58274d53808cf
kamaaki <akihiro16910@gmail.com>
2022-12-30 09:27:31 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  async asyncData({ $config }) {
+    const { data } = await axios.get(
+      "https://kamablog.microcms.io/api/v1/blog",
+      {
+        headers: {
+          "X-MICROCMS-API-KEY": $config.apiKey,
+        },
+      }
+    );
+    return data;
+  },
+};
+</script>
